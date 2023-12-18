b90c2c6c4aa4436f2a8ad9df311a1f9e0aa787e5
Wil van der Tuin <wil@vandertuin.nl>
2023-12-18 12:45:42 -0600

    Style the buttons.

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,7 +1,7 @@
 import { html } from 'htm/preact'
 
 const Button = ({ onClick, children }) => html`
-  <button onClick=${onClick}>${children}</button>
+  <button type="button" onClick=${onClick}>${children}</button>
 `
 
 export default Button
--- app/elements/buttonList/Style.js
+++ app/elements/buttonList/Style.js
@@ -18,15 +18,26 @@ const Style = () => html`<style>
   }
 
   button {
-    padding: 0.5rem;
+    padding: 0.5rem 1rem;
     border-radius: 0.25rem;
     background-color: #eee;
     cursor: pointer;
-    transition: opacity 0.5s ease-in-out;
+    transition: all 0.2s ease, opacity 0.5s ease-in-out;
+    font-weight: 600;
+    color: #666;
+    border: 1px solid #ddd;
+    text-transform: capitalize;
+    letter-spacing: 0.05em;
 
     @starting-style {
       opacity: 0;
     }
+  }
+
+  button:hover {
+    background-color: #ccc;
+    border-color: #bbb;
+    color: #333;
   }
 </style>`
 
