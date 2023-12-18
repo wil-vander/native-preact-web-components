660801075368c8e796a3b6b4525db333acf87701
Wil van der Tuin <wil@vandertuin.nl>
2023-12-18 12:32:43 -0600

    Remove comment.

--- app/elements/buttonList/useButtonList.js
+++ app/elements/buttonList/useButtonList.js
@@ -10,12 +10,11 @@ const useButtonList = () => {
   }, [])
 
   const onClick = (button) => {
+    setButtons([...buttons, `${button} - copy`])
+
     // Trigger an event that can be picked up by the surrounding page.
     const event = new CustomEvent('button:click', { detail: button })
     document.dispatchEvent(event)
-
-    // Update the state.
-    setButtons([...buttons, button])
   }
 
   return {
